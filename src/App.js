19565dc38709aa59ebd5f0047b3a0847f31815a9
rgruevski <robgruevski@gmail.com>
2022-06-16 11:54:57 -0400

    mining

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-const express = require('express');
-const app = express();
 let Block = require("./components/Block");
 let Blockchain = require("./components/Blockchain");
 let Transaction = require("./components/Transaction");
 
-app.get("/blockchain", (req, res) => {
+const express = require('express');
+const { response } = require("express");
+const app = express();
 
-    let transaction = new Transaction("Sebastian", "Rob", 10);
+app.use(express.json());
 
-    let genesisBlock = new Block();
-    let blockchain = new Blockchain(genesisBlock);
+let transactions = [];
+let genesisBlock = new Block();
+let blockchain = new Blockchain(genesisBlock);
 
-    let block = blockchain.getNextBlock([transaction]);
+app.post("/transactions", (req, res) => {
+
+    let to = req.body.to;
+    let from = req.body.from;
+    let amount = req.body.amount;
+
+    let transaction = new Transaction(to, from, amount);
+
+    transactions.push(transaction);
+
+    res.json(transactions);
+});
+
+app.get("/mine", (req, res) => {
+    let block = blockchain.getNextBlock(transactions);
     blockchain.addBlock(block);
+    res.json(block);
+})
 
-    let anotherTransaction = new Transaction("Murphy", "Rob", 100);
-    let anotherBlock = blockchain.getNextBlock([anotherTransaction, transaction]);
-    blockchain.addBlock(anotherBlock);
-
+app.get("/blockchain", (req, res) => {
     res.json(blockchain);
+    // let transaction = new Transaction("Sebastian", "Rob", 10);
+
+    // let genesisBlock = new Block();
+    // let blockchain = new Blockchain(genesisBlock);
+
+    // let block = blockchain.getNextBlock([transaction]);
+    // blockchain.addBlock(block);
+
+    // let anotherTransaction = new Transaction("Murphy", "Rob", 100);
+    // let anotherBlock = blockchain.getNextBlock([anotherTransaction, transaction]);
+    // blockchain.addBlock(anotherBlock);
+
+    // res.json(blockchain);
 })
 
 app.listen(8080, () => {
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,8 +2,8 @@
 class Transaction {
     // The constructor takes in the 3 variables and assigns the values to the newly initialized Transaction Properties.
     constructor(to, from, amount) {
-        this.from = from
         this.to = to
+        this.from = from
         this.amount = amount
     };
 }
